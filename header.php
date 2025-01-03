bd0629c6ad79ec1ac5b187f574172fdd95ad22b0
Aamir Latif <techroof360@gmail.com>
2025-01-03 20:03:48 +0500

    Update navbar responsiveness, css update and add more pages.

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 /**
  * The template for displaying the header
  *
- * This is the template that displays all of the <head> section, opens the <body> tag and adds the site's header.
+ * 
  *
  * @package HelloElementor
  */
